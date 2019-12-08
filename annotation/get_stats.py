5e94cbd110c7b98af0e3862b1ee1099df4e937fd
raphsilva <raphaelsilva2500@gmail.com>
2019-12-08 20:43:12 -0300

    Refactored.

--- annotation/get_stats.py
+++ annotation/get_stats.py
@@ -8,92 +8,58 @@ DIRECTORY_RAW = 'preview'
 confusion_polarity = defaultdict(lambda : defaultdict(int))
 confusion_aspect = defaultdict(lambda : defaultdict(int))
 
-opinion_amount_stats = defaultdict(int)
+opinions_per_sentence = defaultdict(int)
 
 stats_polarities = defaultdict(int)
 stats_aspects = defaultdict(int)
 
-count_sentences = defaultdict(int)
-count_reviews = defaultdict(int)
-
-count_words = 0
-
 for FILENUMBER in ['10', '11', '30', '31']:
-
-    distinct_sentences = set()
-    distinct_reviews = set()
 
     rev_data = json.loads(open(f'{DIRECTORY_REV}/{FILENUMBER}.json').read())
     raw_data = json.loads(open(f'{DIRECTORY_RAW}/{FILENUMBER}.json').read())
 
-    for r in raw_data['data']:
-        distinct_sentences.add(r['sentence_id'])
-        distinct_reviews.add(r['review_id'])
-        count_words += len(r['sentence'].split())
-
-    count_sentences[FILENUMBER] = len(distinct_sentences)
-    count_reviews[FILENUMBER] = len(distinct_reviews)
-
     for d in rev_data['data']:
 
-        amount_opinions = len(d['opinions'])
+        qnt_opinions_in_sentence = len(d['opinions'])
 
-        opinion_amount_stats[amount_opinions] += 1
+        opinions_per_sentence[qnt_opinions_in_sentence] += 1
 
         for r in raw_data['data']:
 
-            ori_pol = r['polarity']
-            ori_asp = r['aspect']
+            guessed_polarity = r['polarity']
+            guessed_aspect = r['aspect'].replace('Á', 'A')
 
             if int(r['sentence_id']) == d['id']:
 
                 for opinion in d['opinions']:
 
-                    rev_pol = opinion[1]
-                    rev_asp = opinion[0].replace('Á', 'A')
+                    revised_polarity = opinion[1]
+                    revised_aspect = opinion[0].replace('Á', 'A')
 
-                    if 'scope' in rev_asp and amount_opinions > 1:
-                        print(FILENUMBER, d['id'])
-                        print(d['opinions'])
-                        print()
+                    if 'A' <= revised_aspect[0] <= 'Z':  # Lowercase aspects do not have meaningful polarity
+                        stats_polarities[revised_polarity] += 1
 
-                    if 'A' <= rev_asp[0] <= 'Z':
-                        stats_polarities[rev_pol] += 1
-                    stats_aspects[rev_asp] += 1
+                    stats_aspects[revised_aspect] += 1
 
-                    if amount_opinions == 1:
+                    if qnt_opinions_in_sentence == 1:
 
-                        confusion_polarity[ori_pol][rev_pol] += 1
+                        confusion_polarity[guessed_polarity][revised_polarity] += 1
 
-                        for asp in ori_asp.split():
-                            confusion_aspect[asp][rev_asp] += 1
+                        for asp in guessed_aspect.split():
+                            confusion_aspect[asp][revised_aspect] += 1
 
 
-    # pprint(raw_data)
-# exit()
-
-pprint(confusion_polarity)
-
-pprint(confusion_aspect)
-
-pprint(opinion_amount_stats)
 
 import pandas as pd
 
 confusion_polarity_matrix = pd.DataFrame(confusion_polarity).T.fillna(0)
-print(confusion_polarity_matrix)
-
 confusion_aspect_matrix = pd.DataFrame(confusion_aspect).T.fillna(0)
+
 confusion_aspect_matrix = confusion_aspect_matrix.reindex(sorted(confusion_aspect_matrix.columns), axis=1)
-# confusion_aspect_matrix = confusion_aspect_matrix.sort_index(by=0, ascending=[True])
-print(confusion_aspect_matrix)
+confusion_polarity_matrix = confusion_polarity_matrix.reindex(sorted(confusion_polarity_matrix.columns), axis=1)
+
+confusion_aspect_matrix.to_csv('aspect_confusion.csv')
+confusion_polarity_matrix.to_csv('polarity_confusion.csv')
 
 
-confusion_aspect_matrix.to_csv('confusion_aspect_matrix.csv')
 
-pprint(stats_aspects)
-pprint(stats_polarities)
-
-pprint(count_sentences)
-pprint(count_reviews)
-pprint(count_words)
